d1590051ecbc5527bee322e071cf9d901ee37f45
Raul Zuniga <rzuniga64@gmail.com>
2016-11-24 19:29:40 -0600

    Remove webpack

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,3 +1,7 @@
+/**
+ *
+ *
+ */
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,7 @@ var router = express.Router();
 var Todo = require('../models/todoModel');
 
 router.get('/', function(req,res, next){
-    Todo.find({}, function(err, docs){
-        todos = [];
-        docs.forEach(function(doc){
-            todos.push(doc.todo);
-        });
-        //console.log(todos);
-        res.render('index', {todos: todos});
-    });
-});
-
-router.post('/', function(req, res, next){
-    var name = req.body.name;
-    var todo = req.body.todo;
-    var newTodo = new Todo({
-        username: name,
-        todo: todo,
-        isDone: true,
-        hasAttachment: true
-    });
-    newTodo.save();
-    res.redirect('/');
+        res.render('index');
 });
 
 module.exports = router;
